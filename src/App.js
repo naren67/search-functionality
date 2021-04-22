2ce50caf398850b5d7413fdd6b2020ba944d80b8
naren67 <naren678n@gmail.com>
2021-04-22 16:48:18 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+import fakeData from './MOCK_DATA.json'
 
 function App() {
+
+  { console.log(fakeData)}
+
+  const [input, setInput] = useState('')
+
+  console.log(input)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+         
+        <div className="app__fakeData">
+          <h1>Fake data</h1>
+        </div>
+
+        <div className="app__input">
+            <input type="text" value={input} onChange={(e)=> {setInput(e.target.value)}}/>
+        </div>
+
+        {fakeData.filter((data)=>{
+          if(input === '')
+          {
+            return data
+          }
+          else if(data.id == input )
+          {
+            return data
+          }
+          else if(data.first_name.toLowerCase().includes(input.toLowerCase()) )
+          {
+            return data
+          }
+          else if(data.last_name.toLowerCase().includes(input.toLowerCase()) )
+          {
+            return data
+          }
+          else if(data.email.toLowerCase().includes(input.toLowerCase()) )
+          {
+            return data
+          }
+          else if(data.gender.toLowerCase().includes(input.toLowerCase()) )
+          {
+            return data
+          }
+          
+        }).map((e)=>(
+          <div className="app__data">
+             <p>{e.id}</p>
+             <p>{e.first_name}</p>
+             <p>{e.last_name}</p>
+             <p>{e.email}</p>
+             <p>{e.gender}</p>
+           </div>
+        ))}
+       
     </div>
   );
 }
